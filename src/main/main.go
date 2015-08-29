8b829694509c3fd3018484cac5030ad05c1062a0
YuichiSawada <y_084743@yahoo.co.jp>
2015-08-30 02:56:12 +0900

    can connect by port

---
+++ src/main/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+)
+
+func routing(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	fmt.Println(r.Form)
+	fmt.Println("path", r.URL.Path)
+	fmt.Println("scheme", r.URL.Scheme)
+	fmt.Println(r.Form["url_long"])
+	for k, v := range r.Form {
+		fmt.Println("kwy:", k)
+		fmt.Println("val:", strings.Join(v, ""))
+	}
+	fmt.Fprintf(w, "Hello go world!!!!")
+}
+
+func main() {
+	http.HandleFunc("/", routing)
+	err := http.ListenAndServe(":9898", nil)
+	if err != nil {
+		log.Fatal("ListenAndServer:", err)
+	}
+}
