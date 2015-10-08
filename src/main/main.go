b7c957e7dfbfc932f5d9c0d9419bd6cd28eb7f05
YuichiSawada <y_084743@yahoo.co.jp>
2015-10-08 23:45:49 +0900

    can insert sign up data

--- src/main/main.go
+++ src/main/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"connectdb"
 	"fmt"
 	_ "github.com/gorilla/mux"
 	_ "html/template"
@@ -12,35 +13,12 @@ import (
 )
 
 func routing(w http.ResponseWriter, r *http.Request) {
-	//r.ParseForm()
-	//fmt.Println(r.Form)
-	//fmt.Println("path", r.URL.Path)
-	//fmt.Println("scheme", r.URL.Scheme)
-	//fmt.Println(r.Form["url_long"])
-	//for k, v := range r.Form {
-	//	fmt.Println("kwy:", k)
-	//	fmt.Println("val:", strings.Join(v, ""))
-	//}
 	fmt.Fprintf(w, "Hello go world!!!!")
 }
 
-/*
-func login(w http.ResponseWriter, r *http.Request) {
-
-		r.ParseForm()
-		if r.Method == "GET" {
-			t, _ := template.ParseFiles("login.gtpl")
-			w.Header().Set("Content-Type", "text/html")
-			t.Execute(w, sess.Get("username"))
-		} else {
-			sess.Set("username", r.Form["username"])
-			http.Redirect(w, r, "/", 302)
-		}
-
-}*/
-
 func main() {
 	//r := mux.NewRouter()
+	connectdb.Opendb()
 	http.HandleFunc("/", routing)
 	http.HandleFunc("/login", login.Server)
 	http.HandleFunc("/signup", signup.Server)
@@ -48,5 +26,4 @@ func main() {
 	if err != nil {
 		log.Fatal("ListenAndServer:", err)
 	}
-	//fmt.Fprintf(w, "hellow")
 }
